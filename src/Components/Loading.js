1c7186726428c08769fddaeed1c088cb899eee27
MValderrama <mavalderrama@gmail.com>
2020-02-07 09:50:54 -0500

    update in file structure

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -5,30 +5,30 @@ import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 
 export default class Loading extends Component {
-    render() {
-        const { open } = this.props;
-        return (
-            <Dialog open={open}>
-                <Paper>
-                    <CircularProgress
-                        style={{
-                            display: "block",
-                            marginLeft: "auto",
-                            marginRight: "auto"
-                        }}
-                    />
-                    <TextField
-                        disabled
-                        id="standard-disabled"
-                        defaultValue="Loading..."
-                        style={{
-                            // display: "block",
-                            marginLeft: "auto",
-                            marginRight: "auto"
-                        }}
-                    />
-                </Paper>
-            </Dialog>
-        );
-    }
+  render() {
+    const { open } = this.props;
+    return (
+      <Dialog open={open}>
+        <Paper>
+          <CircularProgress
+            style={{
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+          />
+          <TextField
+            disabled
+            id="standard-disabled"
+            defaultValue="Loading..."
+            style={{
+              // display: "block",
+              marginLeft: "auto",
+              marginRight: "auto"
+            }}
+          />
+        </Paper>
+      </Dialog>
+    );
+  }
 }
--- src/Redux/Constants/index.js
+++ src/Redux/Constants/index.js
@@ -1,5 +1,4 @@
 export default {
-    // API_URI: "http://127.0.0.1:5000"
-    // API_URI: "http://172.19.128.1"
-    API_URI: "/api/v1"
+  API_URI:
+    process.env.NODE_ENV === "development" ? "http://127.0.0.1:5000" : "/api/v1"
 };
