278c4bba7e8fe7a5bbce479f2335afaa005b1d32
mavalderrama <mavalderrama@outlook.com>
2020-02-06 23:51:43 -0500

    build

---
+++ precache-manifest.6db6f152c3aeeae7276040be465d0ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "97e4334d35e77c6d5995d94b2dfe8d90",
+    url: "/index.html"
+  },
+  {
+    revision: "b548f99a04e25b6c0872",
+    url: "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    revision: "2fa48f5e0181a5208866",
+    url: "/static/js/2.144dd80b.chunk.js"
+  },
+  {
+    revision: "3adc01bea76e7956dc3633ee898f6936",
+    url: "/static/js/2.144dd80b.chunk.js.LICENSE.txt"
+  },
+  {
+    revision: "b548f99a04e25b6c0872",
+    url: "/static/js/main.db241405.chunk.js"
+  },
+  {
+    revision: "4897dcda999fd9f74a5f",
+    url: "/static/js/runtime-main.60197325.js"
+  }
+]);
